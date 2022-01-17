e11e6212c704358f3a1d5e7ab793e919c6101c4f
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-17 07:08:18 +0100

    Using temp folder for installer

--- Windows/Setup-LeagueClient.ps1
+++ Windows/Setup-LeagueClient.ps1
@@ -28,6 +28,8 @@ $REGION_LOWER = $env:LOL_REGION.ToLower()
 $REGION_UPPER = $env:LOL_REGION.ToUpper()
 
 # Config.
+$INSTALLER_EXE = "$env:RUNNER_TEMP\install.$REGION_LOWER.exe"
+
 $RCS_LOCKFILE = "$env:LOCALAPPDATA\Riot Games\Riot Client\Config\lockfile"
 $RCS_DIR = "C:\Riot Games\Riot Client"
 $RCS_EXE = "$RCS_DIR\RiotClientServices.exe"
@@ -98,7 +100,7 @@ If (-Not (Test-Path $LCU_EXE)) {
     $attempts = 5
     While ($True) {
         Try {
-            Invoke-WebRequest "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.$REGION_LOWER.exe" -OutFile "install.$REGION_LOWER.exe"
+            Invoke-WebRequest "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.$REGION_LOWER.exe" -OutFile $INSTALLER_EXE
             Break
         }
         Catch {
@@ -111,7 +113,7 @@ If (-Not (Test-Path $LCU_EXE)) {
         }
     }
     
-    Invoke-Expression ".\install.$REGION_LOWER.exe --skip-to-install"
+    Invoke-Expression "$INSTALLER_EXE --skip-to-install"
 
     # RCS starts, but install of LoL hangs, possibly due to .NET Framework 3.5 missing.
     # So we restart it and then it works.
